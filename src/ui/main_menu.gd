79b0fba9a4c5cc393f4a2f7ca083eab4a3cc9abf
Carl Erosa <carlmelvinerosa3@gmail.com>
2026-04-15 21:42:18 +0800

    feat(ui): update main menu to include load game button functionality

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -3,15 +3,21 @@ extends Control
 @onready var play_button = $MarginContainer/VBoxContainer/PlayButton
 @onready var settings_button = $MarginContainer/VBoxContainer/SettingsButton
 @onready var quit_button = $MarginContainer/VBoxContainer/QuitButton
+@onready var load_button = $MarginContainer/VBoxContainer/LoadButton
 
 func _ready():
 	play_button.pressed.connect(_on_play_pressed)
 	settings_button.pressed.connect(_on_settings_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
+	load_button.pressed.connect(_on_load_pressed)
 
 func _on_play_pressed():
 	# Make sure this file name is exactly correct!
 	get_tree().change_scene_to_file("res://scenes/templates/chapter_1.tscn")
+	
+func _on_load_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/load_game_menu.tscn")
+
 
 func _on_settings_pressed():
 	# Points to the new scene we made in Step 4
