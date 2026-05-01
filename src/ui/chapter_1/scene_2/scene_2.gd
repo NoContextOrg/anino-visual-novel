b9023f53a583a36219de6d16e1b9406eee42dd30
Carlthegreatt <blancaflorcarlferros@gmail.com>
2026-05-01 20:41:58 +0800

    feat(scenes): improve scene dialogue handling

--- src/ui/chapter_1/scene_2/scene_2.gd
+++ src/ui/chapter_1/scene_2/scene_2.gd
@@ -1,6 +1,14 @@
-extends Node2D # (Or whatever type Scene2 is)
+# In scene_5.gd
+extends Node2D
+
+var parser: Node
 
 func _ready():
-	# This runs the exact moment the scene finishes loading!
-	Parser.load_dialogue("res://story/chapter_1/dialogue.json")
-	Parser.start()
+	parser = preload("res://src/gameplay/DialogueParser.gd").new()
+	add_child(parser)
+	
+	# Load scene-specific dialogue
+	parser.load_dialogue("res://story/chapter_1/scene_2/scene_2_dialogue.json")
+	
+	parser.start()
+	
