687a63f44cd9a4ada5333bb8093fe45dd03d230f
Carlthegreatt <blancaflorcarlferros@gmail.com>
2026-05-06 17:17:08 +0800

    chore(git): fetch assets in lfs

--- src/ui/chapter_1/scene_2/scene_2.gd
+++ src/ui/chapter_1/scene_2/scene_2.gd
@@ -1,14 +1,61 @@
-# In scene_5.gd
 extends Node2D
 
-var parser: Node
+@onready var background = $Background
+@onready var sfx_player = $SFXPlayer
+
+var current_bg: Texture2D = null
+var bg_busy := false
+
 
 func _ready():
-	parser = preload("res://src/gameplay/DialogueParser.gd").new()
-	add_child(parser)
-	
-	# Load scene-specific dialogue
-	parser.load_dialogue("res://story/chapter_1/scene_2/scene_2_dialogue.json")
-	
-	parser.start()
-	
+	current_bg = load("res://assets/chapter_1/scene_2/background/inside_tent_bg_1.jpg")
+	background.texture = current_bg
+
+	Parser.load_dialogue("res://story/chapter_1/scene_2/scene_2_dialogue.json")
+	Parser.start()
+
+	EventBus.background_change_requested.connect(_on_bg_change)
+	EventBus.play_sfx_requested.connect(_on_sfx)
+
+
+func _on_bg_change(path: String):
+	print("BG CHANGE CALLED:", path)
+
+	if bg_busy:
+		return
+
+	bg_busy = true
+
+	var new_bg = load(path)
+
+	if new_bg == null:
+		push_error("Failed to load: " + path)
+		bg_busy = false
+		return
+
+	var tween = create_tween()
+
+	tween.tween_property(background, "modulate:a", 0.0, 0.35)
+
+	tween.tween_callback(func():
+		background.texture = new_bg
+		current_bg = new_bg
+	)
+
+	# fade in
+	tween.tween_property(background, "modulate:a", 1.0, 0.35)
+
+	# unlock AFTER animation finishes
+	tween.tween_callback(func():
+		bg_busy = false
+	)
+
+func _on_sfx(path: String):
+	var audio = load(path)
+
+	if audio == null:
+		push_error("Failed to load SFX: " + path)
+		return
+
+	sfx_player.stream = audio
+	sfx_player.play()
