692e0a0033b961b83818c96f1a24c29ea37d00a3
Carlthegreatt <blancaflorcarlferros@gmail.com>
2026-05-10 00:40:27 +0800

    ui(scenes): add fade effect and hover in scene 2

--- src/ui/chapter_1/scene_2/modal_rifle.gd
+++ src/ui/chapter_1/scene_2/modal_rifle.gd
@@ -3,8 +3,10 @@ extends CanvasLayer
 @onready var close_button = $Control/Button
 
 func _ready() -> void:
+	print("[DEBUG] Rifle modal loaded")
 	hide()
 	close_button.pressed.connect(_on_close_button_pressed)
 
 func _on_close_button_pressed() -> void:
+	print("[DEBUG] Close button clicked")
 	hide()
